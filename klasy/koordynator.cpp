7939da6f1e18829c79fcaf628eedde5412ca19fa
artven <rafal.prusak@gmail.com>
2015-05-22 23:26:42 +0200

    pierwszy etap przerobki struktur klas, bo osobne klasy dla pracownikow oraz maszyn sa zjebane

--- klasy/koordynator.cpp
+++ klasy/koordynator.cpp
@@ -8,17 +8,137 @@
 #include "koordynator.h"
 #include <algorithm>
 
+Koordynator::Koordynator(unsigned lP, unsigned lM1, unsigned lM2, unsigned lM3, unsigned int dlSym){
 
-unsigned int Koordynator::PrSz = 0;
-unsigned int Koordynator::PrK  = 0;
-unsigned int Koordynator::LSzM = 0;
-unsigned int Koordynator::LKM  = 0;
-unsigned int Koordynator::KSz  = 0;
-unsigned int Koordynator::KK   = 0;
-unsigned int Koordynator::i    = 0;
-unsigned int Koordynator::j    = 0;
+	std::list<Zlecenie> zleceniaOczekujace = std::list<Zlecenie>();
+	std::list<Zlecenie> zleceniaWykonane   = std::list<Zlecenie>();
+
+    this->PrSz1 = 0; 	//Liczba przetworzonych na maszynie I szaf
+    this->PrSz2 = 0; 	//Liczba przetworzonych na maszynie II szaf
+    this->PrK 	= 0; 	//Liczba przetworzonych na maszynie I krzeseł
+    this->LSzM  = 0;	//Liczba Szaf w magazynie (gotowych)
+    this->LKM 	= 0;	//Liczba krzeseł w magazynie (gotowych)
+    this->KSz 	= 0;	//Liczba szaf czekających w kolejce na rozpoczęcie obróbki
+    this->KK 	= 0;	//Liczba krzeseł oczekujących w kolejce na rozpoczęcie obróbki
+    this->i		= 0;	//liczba zrealizowanych zamówień
+    this->j 	= 0;	//liczba zrealizowanych zamówień
+    this->ZASz 	= 0;	//Zapotrzebowanie na szafy
+    this->ZAK   = 0;	//Zapotrzebowanie na krzesła
+    this->LP	= lP;	//Liczba pracowników w ogóle
+    this->LWP   = lP;	//Liczba wolnych pracowników
+    this->LM1 	= lM1;	//liczba maszyn typu 1
+    this->LM2 	= lM2;	//liczba maszyn typu 2
+    this->LM3 	= lM3;	//liczba maszyn typu 3
+    this->LWM1 	= lM1;	//Liczba wolnych maszyn typu 1
+    this->LWM2 	= lM2;	//Liczba wolnych maszyn typu 2
+    this->LWM3 	= lM3;  //Liczba wolnych maszyn typu 3
 
 
+    this->aktualnyCzas = 0;
+    this->dlugoscSymulacji = dlSym;
+
+	std::list<unsigned> szafyKoniecM3 = std::list<unsigned>();
+	std::list<unsigned> szafyKoniecM2 = std::list<unsigned>();
+	std::list<unsigned> szafyKoniecM1 = std::list<unsigned>();
+
+}
+
+void Koordynator::sim(unsigned n){
+	for(unsigned i=0; i<n; i++){
+
+		if(this->aktualnyCzas == this->aktualnyCzas) break;
+	}
+}
+
+void Koordynator::koniecWyjsciaZlecenia(){
+	//todo
+}
+
+void Koordynator::poczatekWyjsciaZlecenia(){
+	//todo
+}
+
+void Koordynator::poczatekObrobkiMaszynaIII(){
+	if((this->LWP > 0) && (this->LWM3 > 0) && (this->PrSz2 > 0)){
+		this->LWP--;
+		this->LWM3--;
+		this->PrSz2--;
+		this->szafyKoniecM3.push_back(this->aktualnyCzas + this->czasObrobkiSzafyMaszyna3);
+	}
+}
+
+void Koordynator::koniecObrobkiMaszynaIII(){
+	if(this->szafyKoniecM3.front() == this->aktualnyCzas){
+		this->LWP++;
+		this->LWM3++;
+		this->LSzM++;
+		this->szafyKoniecM3.pop_front();
+	}
+}
+
+void Koordynator::poczatekObrobkiSzafMaszynaII(){
+	if((this->LWP > 0) && (this->LWM2 > 0) && (this->PrSz1 > 0)){
+		this->LWP--;
+		this->LWM2--;
+		this->PrSz1--;
+		this->szafyKoniecM2.push_back(this->aktualnyCzas + this->czasObrobkiSzafyMaszyna2);
+	}
+}
+
+void Koordynator::koniecObrobkiSzafMaszynaII(){
+	if(this->szafyKoniecM2.front() == this->aktualnyCzas){
+		this->LWP++;
+		this->LWM2++;
+		this->PrSz2++;
+		this->szafyKoniecM2.pop_front();
+	}
+}
+
+void Koordynator::koniecObrobkiSzafMaszynaI(){
+	if(this->szafyKoniecM2.front() == this->aktualnyCzas){
+		this->LWP++;
+		this->LWM2++;
+		this->PrSz2++;
+		this->szafyKoniecM1.pop_front();
+	}
+}
+
+void Koordynator::poczatekObrobkiSzafMaszynaI(){
+	if((this->LWP > 0) && (this->LWM1 > 0) && (this->KSz > 0)){
+		this->LWP--;
+		this->LWM1--;
+		this->KSz--;
+		this->szafyKoniecM1.push_back(this->aktualnyCzas + this->czasObrobkiSzafyMaszyna1);
+	}
+}
+
+void Koordynator::koniecObrobkiKrzeselMaszynaII(){
+	if(this->krzeslaKoniecM2.front() == this->aktualnyCzas){
+		this->LWP++;
+		this->LWM2++;
+		this->LKM++;
+		this->krzeslaKoniecM2.pop_front();
+	}
+}
+
+void Koordynator::poczatekObrobkiKrzeselMaszynaII(){
+	if((this->LWP > 0) && (this->LWM2 > 0) && (this->PrK > 0)){
+		this->LWP--;
+		this->LWM2--;
+		this->PrK--;
+		this->szafyKoniecM2.push_back(this->aktualnyCzas + this->czasObrobkiSzafyMaszyna2);
+	}
+}
+
+void Koordynator::koniecObrobkiKrzeselMaszynaI(){
+
+}
+
+void Koordynator::poczatekObrobkiKrzeselMaszynaI(){
+
+}
+
+/*
 Koordynator::Koordynator(unsigned int lP, unsigned int lMI, unsigned int lMII, unsigned int lMIII, unsigned int dlSym):
 	dlugoscSymulacji(dlSym), czasSymulacji(0){
 
@@ -64,58 +184,22 @@ void Koordynator::koniecWyjsciaZlecenia(){
 }
 
 void Koordynator::koniecObrobkiMaszynaIII(){
+	unsigned int LWP = (this->pracownicy[0]).get(Pracownik::Parameter::liczbaWolnychPracownikow);
+	unsigned int LWM3 = (this->maszyny[0].get(Maszyna::Parameter::liczbaWolnychMaszynTypu3));
 
+	if((LWP > 0) && (LWM3 > 0) && (Koordynator::Pr2Sz > 0)){
+		// todo trzeba dodać metody dla maszyn i pracowników aby mogli iśc pracować
+		Koordynator::Pr2Sz--;
+	}
 }
 
-void Koordynator::poczatekObrobkiMaszynaIII(){
 
-}
-
-void Koordynator::koniecObrobkiSzafMaszynaII(){
-
-}
-
-void Koordynator::poczatekObrobkiSzafMaszynaII(){
-
-}
-
-void Koordynator::koniecObrobkiSzafMaszynaI(){
-
-}
-
-void Koordynator::poczatekObrobkiSzafMaszynaI(){
-
-}
-
-void Koordynator::koniecObrobkiKrzeselMaszynaII(){
-
-}
-
-void Koordynator::poczatekObrobkiKrzeselMaszynaII(){
-
-}
-
-void Koordynator::koniecObrobkiKrzeselMaszynaI(){
-
-}
-
-void Koordynator::poczatekObrobkiKrzeselMaszynaI(){
-
-}
-
-void Koordynator::poczatekRealizacji(){
-
-}
-
-void Koordynator::przybycieZlecenia(){
-
-}
 
 void Koordynator::symuluj(unsigned int n){
 	for(unsigned i=0; i<n; i++){
 		this->czasSymulacji++;
 
-		this->poczatekWyjsciaZlecenia();
+		this->poczatekWyjsciaZlecenia();		//todo tutaj została zamieniona kolejność #Artur!!
 		this->koniecWyjsciaZlecenia();
 
 		this->koniecObrobkiMaszynaIII();
@@ -135,3 +219,4 @@ void Koordynator::symuluj(unsigned int n){
 	}
 
 }
+*/
--- klasy/koordynator.h
+++ klasy/koordynator.h
@@ -16,17 +16,55 @@
 #include "pracownik.h"
 #include "zlecenie.h"
 
-
+enum class Parameter{
+    PrSz, 	//Liczba przetworzonych na maszynie I szaf
+    PrSz2, 	//Liczba przetworzonych na maszynie II szaf
+    PrK, 	//Liczba przetworzonych na maszynie I krzeseł
+    LSzM,	//Liczba Szaf w magazynie (gotowych)
+    LKM,	//Liczba krzeseł w magazynie (gotowych)
+    KSz,	//Liczba szaf czekających w kolejce na rozpoczęcie obróbki
+    KK,		//Liczba krzeseł oczekujących w kolejce na rozpoczęcie obróbki
+    i,		//liczba zrealizowanych zamówień
+    j,		//liczba zamówień, które przybyły do systemu
+    LWP,	//Liczba wolnych pracowników
+    ZASz,	//Zapotrzebowanie na szafy
+    ZAK,	//Zapotrzebowanie na krzesła
+    LWM1,	//Liczba wolnych maszyn I
+    LWM2,	//Liczba wolnych maszyn II
+    LWM3,	//Liczba wolnych maszyn III
+    lPracownikow,
+    lMaszyn1,
+    lMaszyn2,
+    lMaszyn3,
+    aktualnyCzas
+};
 
 /**
  * klasa koordynatora zadaniowego który ogarnia cały ten burdel
  * */
 class Koordynator{
 	public:
-		Koordynator(unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+		/*
+		 * konstruktor Koordynatora obsługującego zakład
+		 * @param lP liczba pracowników w zakkładzie
+		 * @param lM1 liczba maszyn typu pierwszego
+		 * @param lM2 liczba maszyn typu drugiego
+		 * @param lM3 liczba maszyn typu trzeciego
+		 * @param dlSym długość symulacji podana w jednostkach czasu
+		 * */
+		Koordynator(unsigned lP, unsigned lM1, unsigned lM2, unsigned lM3, unsigned int dlSym);
 
-		void symuluj(unsigned int);
+		/*
+		 * przprowadza symulację trwającą n jednostek czasu
+		 * jeżli czas symulacji przekroczy długość symulacji funkcja jest przerywana
+		 * @param n ilosc jednostek czasu
+		 */
+		void sim(unsigned n);
 
+		void set(Parameter, unsigned);
+		unsigned int set(Parameter);
+
+	private:
 		//nagłówki rozpatrywane przez koordynatora
 		void koniecWyjsciaZlecenia();
 		void poczatekWyjsciaZlecenia();
@@ -43,34 +81,54 @@ class Koordynator{
 		void poczatekRealizacji();
 		void przybycieZlecenia();
 
-		//dla W
-		enum class Parameter{
-
-		};
-
-		void set(Parameter, unsigned int);
-		unsigned int set(Parameter);
-
-
-	private:
 		//"globalne zmienne stanu"
-		static unsigned int PrSz;	//liczba szaf przetworzonych na maszynie typu I
-		static unsigned int PrK;	//liczba krzeseł przetworzonych na maszynie typu I
-		static unsigned int LSzM;	//liczba szaf w magazynie(gotowych)
-		static unsigned int LKM;	//liczba krzeseł w magazynie(gotowych)
-		static unsigned int KSz;	//liczba szaf czekających w kolejce na rozpoczęcie obróbki
-		static unsigned int KK;     //liczba krzeseł oczekujących w kolejce na rozpoczęcie obróbki
-		static unsigned int i;		//numer aktualnie realizowanego zamówienia
-		static unsigned int j;		//pokazuje ilość zamówień, które przybyły do systemu
+	    unsigned PrSz1; //Liczba przetworzonych na maszynie I szaf
+		unsigned PrSz2; //Liczba przetworzonych na maszynie II szaf
+		unsigned PrK; 	//Liczba przetworzonych na maszynie I krzeseł
+		unsigned LSzM;	//Liczba Szaf w magazynie (gotowych)
+		unsigned LKM;	//Liczba krzeseł w magazynie (gotowych)
+		unsigned KSz;	//Liczba szaf czekających w kolejce na rozpoczęcie obróbki
+		unsigned KK;	//Liczba krzeseł oczekujących w kolejce na rozpoczęcie obróbki
+		unsigned i;		//liczba zrealizowanych zamówień
+		unsigned j;		//liczbazamówień, które przybyły do systemu
+		unsigned LWP;	//Liczba wolnych pracowników
+		unsigned ZASz;	//Zapotrzebowanie na szafy danego zamówienia
+		unsigned ZAK;	//Zapotrzebowanie na krzesła danego zamówienia
+		unsigned LWM1;	//Liczba wolnych maszyn I
+		unsigned LWM2;	//Liczba wolnych maszyn II
+		unsigned LWM3;	//Liczba wolnych maszyn III
+		unsigned LP;	//Liczba pracowników w ogóle
+		unsigned LM1;	//liczba maszyn typu 1
+		unsigned LM2;	//liczba maszyn typu 2
+		unsigned LM3;	//liczba maszyn typu 3
 
+		unsigned aktualnyCzas;
+		unsigned dlugoscSymulacji;
 
-		unsigned int dlugoscSymulacji;
-		unsigned int czasSymulacji;
+		/*
+		 * Czasy obsługi na maszynach
+		 * todo narazie będą stałymi, kiedyś odjebie się losowanko
+		 * */
+		const unsigned czasObrobkiSzafyMaszyna3 = 30;
+		const unsigned czasObrobkiSzafyMaszyna2 = 5;
+		const unsigned czasObrobkiSzafyMaszyna1 = 15;
+		const unsigned czasObrobkiKrzeslaMaszyna2 = 10;
+		const unsigned czasObrobkiKrzeslaMaszyna1 = 20;
 
-		std::vector<Pracownik> pracownicy;
-		std::vector<Maszyna> maszyny;
-		std::list<Zlecenie> zlecenia;
+//		std::vector<Pracownik> pracownicy;
+//		std::vector<Maszyna> maszyny;
 
+		std::list<Zlecenie> zleceniaOczekujace;
+		std::list<Zlecenie> zleceniaZrealizowane;
+
+		//czasy końca obórki szaf na maszynach
+		std::list<unsigned> szafyKoniecM3;
+		std::list<unsigned> szafyKoniecM2;
+		std::list<unsigned> szafyKoniecM1;
+
+		//czasy końca obróbki krzeseł na maszynasz
+		std::list<unsigned> krzeslaKoniecM2;
+		std::list<unsigned> krzeslaKoniecM1;
 
 };
 
