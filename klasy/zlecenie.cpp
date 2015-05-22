30bf720fdbe292a8327bccdfd1b0f30e3936eb14
artven <rafal.prusak@gmail.com>
2015-05-22 06:58:34 +0200

    zarys klasy zlecenia

---
+++ klasy/zlecenie.cpp
@@ -0,0 +1,91 @@
+/*
+ * zlecenie.cpp
+ *
+ *  Created on: 22 maj 2015
+ *      Author: rafal
+ */
+
+#include <iostream>
+
+unsigned int Zlecenie::id = 1;
+unsigned int Zlecenie::lz = 0;
+
+Zlecenie::Zlecenie():Nr(Zlecenie::id), CzP(0), CzW(0), ZAKs(0), ZAK(0){
+	Zlecenie::id++;
+	Zlecenie::lz++;
+}
+
+Zlecenie::Zlecenie(const Zlecenie& z):Nr(z.Nr), CzP(z.CzP), CzW(z.CzW), ZAKs(z.ZAKs), ZAK(z.ZAK){
+	Zlecenie::lz++;
+}
+
+Zlecenie::~Zlecenie(){
+	Zlecenie::lz--;
+}
+
+Zlecenie& Zlecenie::operator=(const Zlecenie & z){
+	this->Nr = z.Nr;
+	this->CzP = z.CzP;
+	this->CzW = z.CzW;
+	this->ZAKs = z.ZAKs;
+	this->ZAK = z.ZAK;
+
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream output, const Zlecenie& z){
+	output << "Zlecenie nr=" << z.Nr << " parametry=" << z.CzP << " " << z.CzW << " " << z.ZAKs << " " << z.ZAK;
+	return output;
+}
+
+void Zlecenie::set(Parameter param, unsigned int val){
+	switch(Parameter){
+		case Parameter::nrZlecenia:
+			this->Nr = val;
+			break;
+
+		case Parameter::czasPrzybyciaZlecenia:
+			this->CzP = val;
+			break;
+
+		case Parameter::czasWyjsciaZlecenia:
+			this->CzW = val;
+			break;
+
+		case Parameter::zapotrzebowanieNaSzsafy:
+			this->ZAKs = val;
+			break;
+
+		case Parameter::zapotrzebowanieNaKrzesła:
+			this->ZAK = val;
+			break;
+	}
+}
+
+unsigned int Zlecenie::get(Parameter){
+	switch(Parameter){
+		case Parameter::nrZlecenia:
+			return this->Nr;
+			break;
+
+		case Parameter::czasPrzybyciaZlecenia:
+			return this->CzP;
+			break;
+
+		case Parameter::czasWyjsciaZlecenia:
+			return this->CzW;
+			break;
+
+		case Parameter::zapotrzebowanieNaSzsafy:
+			return this->ZAKs;
+			break;
+
+		case Parameter::zapotrzebowanieNaKrzesła:
+			return this->ZAK;
+			break;
+
+		default:
+			return 0;
+			break;
+	}
+}
---
+++ klasy/zlecenie.h
@@ -0,0 +1,46 @@
+/*
+ * zlecenie.h
+ *
+ *  Created on: 21 maj 2015
+ *      Author: rafal
+ */
+
+#ifndef KLASY_ZLECENIE_H_
+#define KLASY_ZLECENIE_H_
+
+#include <iostream>
+
+class Zlecenie{
+	public:
+		Zlecenie();
+		Zlecenie(const Zlecenie&);
+		~Zlecenie();
+		Zlecenie& operator=(const Zlecenie &);
+		friend std::ostream& operator<<(std::ostream, const Zlecenie&);
+
+		//dla W
+		enum class Parameter{
+			nrZlecenia,
+			czasPrzybyciaZlecenia,
+			czasWyjsciaZlecenia,
+			zapotrzebowanieNaSzsafy,
+			zapotrzebowanieNaKrzesła
+		};
+		void set(Parameter, unsigned int);
+		unsigned int get(Parameter);
+
+	private:
+		static unsigned int id;		//numery identyfikacyjne zleceń
+		static unsigned int lz;		//liczba zleceń
+
+		//parametery
+		unsigned int Nr;	//numer danego zamówienia(priorytet)
+		unsigned int CzP;	//czas przybycia zlecenia
+		unsigned int CzW;	//czas wyjscia zlecenia z systemu
+		unsigned int ZAKs;	//zapotrzebowanie na szafy
+		unsigned int ZAK;	//zapotrzebowanie na kaszesła
+
+};
+
+
+#endif /* KLASY_ZLECENIE_H_ */
