05ac9680804ccc615b0afffb5ef7496dd535dde8
artven <rafal.prusak@gmail.com>
2015-05-22 13:44:07 +0200

    zarys klasy koordynatora

---
+++ klasy/koordynator.cpp
@@ -0,0 +1,22 @@
+/*
+ * koordynator.cpp
+ *
+ *  Created on: 22 maj 2015
+ *      Author: rafal
+ */
+
+#include "koordynator.h"
+
+unsigned int Koordynator::PrSz = 0;
+unsigned int Koordynator::PrK  = 0;
+unsigned int Koordynator::LSzM = 0;
+unsigned int Koordynator::LKM  = 0;
+unsigned int Koordynator::KSz  = 0;
+unsigned int Koordynator::KK   = 0;
+unsigned int Koordynator::i    = 0;
+unsigned int Koordynator::j    = 0;
+
+
+
+Koordynator::Koordynator(unsigned int, unsigned int, unsigned int);
+
---
+++ klasy/koordynator.h
@@ -0,0 +1,60 @@
+/*
+ * koordynator.h
+ *
+ *  Created on: 22 maj 2015
+ *      Author: rafal
+ */
+
+#ifndef KLASY_KOORDYNATOR_H_
+#define KLASY_KOORDYNATOR_H_
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+#include "maszyna.h"
+#include "pracownik.h"
+#include "zlecenie.h"
+
+
+
+/**
+ * klasa koordynatora zadaniowego który ogarnia cały ten burdel
+ * */
+class Koordynator{
+	public:
+		Koordynator(unsigned int, unsigned int, unsigned int);
+
+		//dla W
+		enum class Parameter{
+
+		};
+
+		void set(Parameter, unsigned int);
+		unsigned int set(Parameter);
+
+	private:
+		//"globalne zmienne stanu"
+		static unsigned int PrSz;	//liczba szaf przetworzonych na maszynie typu I
+		static unsigned int PrK;	//liczba krzeseł przetworzonych na maszynie typu I
+		static unsigned int LSzM;	//liczba szaf w magazynie(gotowych)
+		static unsigned int LKM;	//liczba krzeseł w magazynie(gotowych)
+		static unsigned int KSz;	//liczba szaf czekających w kolejce na rozpoczęcie obróbki
+		static unsigned int KK;     //liczba krzeseł oczekujących w kolejce na rozpoczęcie obróbki
+		static unsigned int i;		//numer aktualnie realizowanego zamówienia
+		static unsigned int j;		//pokazuje ilość zamówień, które przybyły do systemu
+
+
+		unsigned int dlugoscSymulacji;
+		unsigned int czasSymulacji;
+
+		std::vector<Pracownik> pracownicy;
+		std::vector<Maszyna> maszyny;
+		std::list<Zlecenie> zlecenia;
+
+
+};
+
+
+
+#endif /* KLASY_KOORDYNATOR_H_ */
